f940983dc048de5aa2a6627775319e4998b0f79a
Muhd Danish <mdanish1908@gmail.com>
2019-07-24 19:04:53 +0800

    Update db.php

--- config/db.php
+++ config/db.php
@@ -1,14 +1,24 @@
 <?php
 
+if (YII_ENV_DEV) {
+    return [
+        'class' => 'yii\db\Connection',
+        'dsn' => 'mysql:host=localhost;dbname=hris',
+        'username' => 'root',
+        'password' => '',
+        'charset' => 'utf8',
+
+        // Schema cache options (for production environment)
+        //'enableSchemaCache' => true,
+        //'schemaCacheDuration' => 60,
+        //'schemaCache' => 'cache',
+    ];
+}
+
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=127.0.0.1;dbname=hris',
     'username' => 'root',
-    'password' => '',
+    'password' => 'h@rw00d',
     'charset' => 'utf8',
-
-    // Schema cache options (for production environment)
-    //'enableSchemaCache' => true,
-    //'schemaCacheDuration' => 60,
-    //'schemaCache' => 'cache',
 ];
